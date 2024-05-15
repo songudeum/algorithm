5cae1dd1c7b4198f3f426cdbed1130626328d31f
Suyeon Kim <128280275+songudeum@users.noreply.github.com>
2024-05-15 20:54:32 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,17 +1,8 @@
 function solution(citations) {
-    //논문 두 범위로 나뉘는 기준점 h번 이상 인용된게 h편 이상이면서 h번 이하 인용되어야 함
-    //먼저 가장 작은값이랑 높은 값의 중간값을 구함
-    //중간값을 토대로 중간값보다 큰 원소의 개수를 구해서 중간값과 일치하면 그대로 반환
-    //일치하지 않으면 값을 1개씩 낮춰서 일치하는지 확인 후 반환?
-    var answer = 0;
-    citations.sort((a,b)=>a-b)
-    const half = Math.round((citations[0]+citations[citations.length-1])/2)
-    for(let i = half; i>0; i--){
-        let length = 0
-        for(const number of citations){
-         if(number >= i) length++   
-        }
-        if(length >= i) return i
-    }
-    return answer;
+     citations = citations.sort((a,b)=>b-a);
+     var i = 0;
+     while(i + 1 <= citations[i]){ //0인덱스부터 개수 이상인지 체크
+         i++;
+     }
+     return i;
 }
